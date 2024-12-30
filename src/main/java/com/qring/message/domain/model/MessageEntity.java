97a0837737d321019edef3d215b677f7f2467a78
Yang ye lin <oky07031217@gmail.com>
2024-12-30 12:59:24 +0900

    [ feat ] 메시지 엔티티 생성 ( #1 )

---
+++ src/main/java/com/qring/message/domain/model/MessageEntity.java
@@ -0,0 +1,55 @@
+package com.qring.message.domain.model;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_slack")
+public class MessageEntity {
+
+    @Id @Tsid
+    @Column(name = "slack_id")
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "modified_at" , nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Column(name = "deleted_by")
+    private String deletedBy;
+
+    @Builder
+    public MessageEntity (Long userId, String content) {
+        this.userId = userId;
+        this.content = content;
+    }
+}
