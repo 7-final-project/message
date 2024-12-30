de0437170a831b974f38e2e018b5c0bc3235c54a
Yang ye lin <oky07031217@gmail.com>
2024-12-30 21:34:57 +0900

    [ feat ] AI 검색 API 개발 ( #3 )

--- src/main/java/com/qring/message/presentation/v1/controller/AiControllerV1.java
+++ src/main/java/com/qring/message/presentation/v1/controller/AiControllerV1.java
@@ -2,10 +2,18 @@ package com.qring.message.presentation.v1.controller;
 
 import com.qring.message.application.global.dto.ResDTO;
 import com.qring.message.application.v1.res.AiPostResDTOV1;
+import com.qring.message.application.v1.res.AiSearchResDTOV1;
 import com.qring.message.domain.model.AiEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/ais")
@@ -28,6 +36,41 @@ public class AiControllerV1 {
                         .data(AiPostResDTOV1.of(dummyAiEntity))
                         .build(),
                 HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<ResDTO<AiSearchResDTOV1>> searchBy(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                                                             @RequestParam(name = "question", required = false) String question,
+                                                             @RequestParam(name = "answer", required = false) String answer,
+                                                             @RequestParam(name = "sort", required = false) String sort) {
+
+        // 더미데이터 ----------------------------------------------
+        List<AiEntity> dummyAis = List.of(
+                AiEntity.builder()
+                        .question("양식 카테고리의 음식점 추천해주세요.")
+                        .answer("파스타집")
+                        .build(),
+                AiEntity.builder()
+                        .question("중식 카테고리의 음식점 추천해주세요.")
+                        .answer("짬뽕집")
+                        .build(),
+                AiEntity.builder()
+                        .question("일식 카테고리의 음식점 추천해주세요.")
+                        .answer("초밥집")
+                        .build()
+        );
+
+        Page<AiEntity> dummyPage = new PageImpl<>(dummyAis, pageable, dummyAis.size());
+        // 추후 삭제 ----------------------------------------------
+
+        return new ResponseEntity<>(
+                ResDTO.<AiSearchResDTOV1>builder()
+                        .code(HttpStatus.OK.value())
+                        .message("AI 검색에 성공했습니다.")
+                        .data(AiSearchResDTOV1.of(dummyPage))
+                        .build(),
+                HttpStatus.OK
         );
     }
 }
