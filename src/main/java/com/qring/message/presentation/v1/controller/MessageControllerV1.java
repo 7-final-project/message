2b90b4a444710d2c11b88e6c07a69eecf3ea27f9
afoisl <160198328+afoisl@users.noreply.github.com>
2025-01-10 12:50:41 +0900

    [ refactor ] MessageController 수정 ( #8 )

--- src/main/java/com/qring/message/presentation/v1/controller/MessageControllerV1.java
+++ src/main/java/com/qring/message/presentation/v1/controller/MessageControllerV1.java
@@ -3,7 +3,9 @@ package com.qring.message.presentation.v1.controller;
 import com.qring.message.application.global.dto.ResDTO;
 import com.qring.message.application.v1.res.MessageGetByIdResDTOV1;
 import com.qring.message.application.v1.res.MessageSearchResDTOV1;
+import com.qring.message.application.v1.service.MessageServiceV1;
 import com.qring.message.domain.model.MessageEntity;
+import com.qring.message.infrastructure.messaging.kafka.KafkaMessageConsumerV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -21,13 +23,15 @@ import java.util.List;
 @RequestMapping("/v1/messages")
 public class MessageControllerV1 {
 
+    private final MessageServiceV1 messageServiceV1;
+
     // 삭제 예정
-//    private final MessageQueueServiceV1 messageQueueServiceV1;
+//    private final KafkaMessageConsumerV1 messageConsumerV1;
 //
 //    @PostMapping("/{email}")
 //    public ResponseEntity<ResDTO<Object>> postBy(@PathVariable String email) {
 //
-//        messageQueueServiceV1.sendMessageToUser(email);
+//        messageConsumerV1.sendMessageToUser(email);
 //
 //        return new ResponseEntity<>(
 //                ResDTO.builder()
@@ -39,80 +43,18 @@ public class MessageControllerV1 {
 //    }
 
     @GetMapping
-    public ResponseEntity<ResDTO<MessageSearchResDTOV1>> searchBy(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+    public ResponseEntity<ResDTO<MessageSearchResDTOV1>> searchBy(@RequestHeader("X-Passport-Token") String passport,
+                                                                  @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
                                                                   @RequestParam(name = "userId", required = false) Long userId,
-                                                                  @RequestParam(name = "content", required = false) String content,
+                                                                  @RequestParam(name = "id", required = false) Long id,
                                                                   @RequestParam(name = "sort", required = false) String sort) {
 
-        // 더미데이터 ----------------------------------------------
-        List<MessageEntity> dummyMessages = List.of(
-                MessageEntity.builder()
-                        .userId(1L)
-                        .content("대기 등록이 완료 되었습니다."
-                                + "예약 인원 : 2명"
-                                + "대기 번호 : 30번"
-                                + "내 앞 대기팀 : 2팀")
-                        .build(),
-                MessageEntity.builder()
-                        .userId(2L)
-                        .content("대기 등록이 완료 되었습니다."
-                                + "예약 인원 : 4명"
-                                + "대기 번호 : 3번"
-                                + "내 앞 대기팀 : 1팀")
-                        .build(),
-                MessageEntity.builder()
-                        .userId(1L)
-                        .content("대기 등록이 완료 되었습니다."
-                                + "예약 인원 : 6명"
-                                + "대기 번호 : 59번"
-                                + "내 앞 대기팀 : 10팀")
-                        .build()
-        );
-
-        Page<MessageEntity> dummyPage = new PageImpl<>(dummyMessages, pageable, dummyMessages.size());
-        // 추후 삭제 ----------------------------------------------
 
         return new ResponseEntity<>(
                 ResDTO.<MessageSearchResDTOV1>builder()
                         .code(HttpStatus.OK.value())
                         .message("메시지 검색에 성공했습니다.")
-                        .data(MessageSearchResDTOV1.of(dummyPage))
-                        .build(),
-                HttpStatus.OK
-        );
-    }
-
-    @GetMapping("/{slackId}")
-    public ResponseEntity<ResDTO<MessageGetByIdResDTOV1>> getBy(@PathVariable Long slackId) {
-
-        // 더미데이터 ----------------------------------------------
-        MessageEntity dummyMessageEntity = MessageEntity.builder()
-                .userId(1L)
-                .content("대기 등록이 완료 되었습니다."
-                        + "예약 인원 : 2명"
-                        + "대기 번호 : 30번"
-                        + "내 앞 대기팀 : 2팀")
-                .build();
-        // 추후 삭제 ----------------------------------------------
-
-        return new ResponseEntity<>(
-                ResDTO.<MessageGetByIdResDTOV1>builder()
-                        .code(HttpStatus.OK.value())
-                        .message("메시지 상세 조회에 성공했습니다.")
-                        .data(MessageGetByIdResDTOV1.of(dummyMessageEntity))
-                        .build(),
-                HttpStatus.OK
-        );
-    }
-
-    @DeleteMapping("/{slackId}")
-    public ResponseEntity<ResDTO<Object>> deleteBy(@RequestHeader("X-User-Id") Long userId,
-                                                   @PathVariable Long slackId) {
-
-        return new ResponseEntity<>(
-                ResDTO.builder()
-                        .code(HttpStatus.OK.value())
-                        .message("메시지 삭제에 성공했습니다.")
+                        .data(messageServiceV1.searchBy(passport, pageable, userId, id, sort))
                         .build(),
                 HttpStatus.OK
         );
