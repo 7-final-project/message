a38a5f30b55607d55e95fbbd72755b0b31b2ad6d
afoisl <160198328+afoisl@users.noreply.github.com>
2025-01-11 01:54:03 +0900

    [ refactor ] 안쓰는 주석 삭제 ( #8 )

--- src/main/java/com/qring/message/infrastructure/repository/JpaReservationRepository.java
+++ src/main/java/com/qring/message/infrastructure/repository/JpaReservationRepository.java
@@ -3,8 +3,5 @@ package com.qring.message.infrastructure.repository;
 import com.qring.message.domain.model.MessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface JpaReservationRepository extends JpaRepository<MessageEntity, Long> {
-
 }
